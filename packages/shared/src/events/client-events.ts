cf40bc06723685d98858f22dd6afc69c389cda41
alazar <alazarfikadu15@gmail.com>
2026-08-06 18:15:35 +0300

    created feature(shared) events

---
+++ packages/shared/src/events/client-events.ts
@@ -0,0 +1,7 @@
+export enum ClientEvents {
+  PLAYER_JOIN = "player:join",
+  PLAYER_MOVE = "player:move",
+  PLAYER_SPLIT = "player:split",
+  PLAYER_EJECT = "player:eject",
+  PLAYER_DISCONNECT = "player:disconnect",
+}
---
+++ packages/shared/src/events/server-events.ts
@@ -0,0 +1,10 @@
+export enum ServerEvents {
+  GAME_INIT = "game:init",
+  GAME_UPDATE = "game:update",
+  PLAYER_JOINED = "player:joined",
+  PLAYER_LEFT = "player:left",
+  PLAYER_DIED = "player:died",
+  LEADERBOARD_UPDATE = "leaderboard:update",
+  GAME_OVER = "game:over",
+  ERROR = "error",
+}
